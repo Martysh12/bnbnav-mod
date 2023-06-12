173f02287bb594f469560ea5ddcf621636d3a5fd
Martin K <martyshkon@gmail.com>
2023-06-13 02:16:13 +0300

    Initial commit :grin:

---
+++ remappedSrc/com/martysh12/bnbnav/client/BnbnavModClient.java
@@ -0,0 +1,13 @@
+package com.martysh12.bnbnav.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BnbnavModClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/martysh12/bnbnav/BnbnavMod.java
@@ -0,0 +1,13 @@
+package com.martysh12.bnbnav;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BnbnavMod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
